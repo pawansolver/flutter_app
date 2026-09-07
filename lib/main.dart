57b19d596c4497d66cf37aab6736253d505cad8e
Mahendra Kumar <mahendrakumaruohyd@gmail.com>
2026-09-07 18:15:46 +0530

    chore: initialize cross-platform plugin registries and update project dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'modules/splash/splash_screen.dart';
 import 'services/notification_service.dart';
